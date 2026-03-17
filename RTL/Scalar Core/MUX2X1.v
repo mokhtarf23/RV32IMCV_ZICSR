cb195588fd031212af7b3edd7daa9391a668e92e
Daze3 <89325411+Daze3@users.noreply.github.com>
2026-03-17 17:21:40 +0100

    Delete RTL/Scalar Core directory

--- RTL/Scalar Core/MUX2X1.v
+++
@@ -1,8 +0,0 @@
-module MUX2X1(input  [31:0] in1, input  [31:0] in2, input sel, output reg [31:0] out);
-    always @(*) begin
-        case(sel)
-            0:out=in1;
-            1:out=in2;
-        endcase 
-    end
-endmodule
