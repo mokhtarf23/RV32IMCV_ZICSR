31cdf328ac304071aab67d4281c810b18c218e11
Mokhtar Fadlelmawla <89325411+mokhtarf23@users.noreply.github.com>
2026-03-26 02:12:15 +0100

    Update VRF.sv

--- RTL/Vector Co-Processor/VRF.sv
+++ RTL/Vector Co-Processor/VRF.sv
@@ -1,28 +1,23 @@
 module vector_regfile #(
-  parameter VLEN      = 128,   // maximum vector register width in bits
-  parameter LGREGS    = 5,     // log2(number of vector registers) = 5 ⇒ 32 regs
-  parameter NREGS     = 32
+  parameter VLEN = 128,   // maximum vector register width in bits
+  parameter LGREGS = 5,     // log2(number of vector registers) = 5 ⇒ 32 regs
+  parameter NREGS = 32
 )(
   input clk,
   input rst,
 
-  // ----- WRITE PORT -----
-  input                    wen,
-  input  [LGREGS-1:0]      waddr,
-  input  [VLEN-1:0]        wdata,
+  input  wen,
+  input  [LGREGS-1:0]  waddr,
+  input  [VLEN-1:0]  wdata,
 
-  // ----- READ PORT 0 (vs1) -----
 
-  input  [LGREGS-1:0]      raddr0,
-  output [VLEN-1:0]        rdata0,
+  input  [LGREGS-1:0] raddr0,
+  output [VLEN-1:0]  rdata0,
 
-  // ----- READ PORT 1 (vs2) -----
-  input  [LGREGS-1:0]      raddr1,
-  output [VLEN-1:0]        rdata1,
+  input  [LGREGS-1:0] raddr1,
+  output [VLEN-1:0] rdata1,
 
-  // ----- READ PORT 2 (mask / v0) -----
-  // Vector‐mask always comes from v0 by the spec, but we still expose a full port
-  output [VLEN-1:0]        rdata2
+  output [VLEN-1:0] rdata2
 );
 
   reg [VLEN-1:0] regs [0:NREGS-1];
@@ -33,11 +28,7 @@ module vector_regfile #(
       i=0;
       for (i = 0; i < NREGS; i=i+1) begin
         regs[i] <= 0;
-         // regs[i] <= {26'd0,i,26'd0,i,26'd0,i,26'd0,i};//32
-        //  regs[i] <= {10'd0,i,10'd0,i,10'd0,i,10'd0,i,10'd0,i,10'd0,i,10'd0,i,10'd0,i}; //16
       end  
-        regs[1] <= {8'd2,8'd4,8'd6,8'd8,8'd10,8'd12,8'd14,8'd16,8'd18,8'd20,8'd22,8'd24,8'd26,8'd28,8'd30,8'd32};
-        regs[2] <= {8'd2,8'd4,8'd6,8'd8,8'd10,8'd12,8'd14,8'd16,8'd18,8'd20,8'd22,8'd24,8'd26,8'd28,8'd30,8'd32};
     end else if (wen) begin
       regs[waddr] <= wdata;
     end
